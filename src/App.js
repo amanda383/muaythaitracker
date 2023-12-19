37294a673f0a1139a780c6c20d4119eaaf188509
Amanda Li <a383li@uwaterloo.ca>
2023-12-19 17:06:47 -0500

    centered camera and cavas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 //install dependencies
 //import dependiences
 //setup webcam and canvas
-//define
+//define references
+//load posenet
+//detect function
+//drawing utlities from tensorflow
+//draww functions
 
 import React, {useRef} from 'react';
 import './App.css';
@@ -13,7 +17,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello
+        <Webcam style ={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right:0,
+          textAlign: "center",
+          zIndex: 9,
+          width:640,
+          height:480
+        }}/>
+
+        <canvas style ={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right:0,
+          textAlign: "center",
+          zIndex: 9,
+          width:640,
+          height:480
+        }}/>
+
+
       </header>
     </div>
   );
