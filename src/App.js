cac3a85534ddf098d5682b55c6b611f4ce535069
Amanda Li <a383li@uwaterloo.ca>
2023-12-19 15:56:58 -0500

    installed dependencies and imported them

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//install dependencies
+//import dependiences
+//setup webcam and canvas
+//define
+
+import React, {useRef} from 'react';
+import './App.css';
+import * as tf from "@tensorflow/tfjs"
+import * as posenet from "@tensorflow-models/posenet"
+import Webcam from 'react-webcam';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
